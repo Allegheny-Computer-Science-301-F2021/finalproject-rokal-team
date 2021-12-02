2434f2ac063b7eeab80e4281e5ddfe071c8df784
Kaleigh White <whitek2@allegheny.edu>
2021-12-01 19:33:16 -0500

    created datasets for specific states and months

--- src/analysis.r
+++ src/analysis.r
@@ -35,14 +35,31 @@ data_zona <- filter(data, STATE_NAME == "Arizona")
 data_wash <- filter(data, STATE_NAME == "Washington")
 data_miss <- filter(data, STATE_NAME == "Missouri")
 
-# pulling just the data for March 2021
-dat_mar2021 <- data %>% filter(Month == "3", Year == "2021")
-View(dat_mar2021)
+# pulling the data for March 2020
+data_zona_mar2020 <- data_zona %>% filter(Month == "3", Year == "2020")
+View(data_zona_mar2020)
+data_wash_mar2020 <- data_wash %>% filter(Month == "3", Year == "2020")
+View(data_wash_mar2020)
+data_miss_mar2020 <- data_miss %>% filter(Month == "3", Year == "2020")
+View(data_miss_mar2020)
 
-# pulling just the data for February 2021
-dat_feb2021 <- data %>% filter(Month == "2", Year == "2021")
-View(dat_feb2021)
+# pulling the data for August 2020
+data_zona_aug2020 <- data_zona %>% filter(Month == "8", Year == "2020")
+View(data_zona_aug2020)
+data_wash_aug2020 <- data_wash %>% filter(Month == "8", Year == "2020")
+View(data_wash_aug2020)
+data_miss_aug2020 <- data_miss %>% filter(Month == "8", Year == "2020")
+View(data_miss_aug2020)
 
+# pulling data for December 2020
+data_zona_dec2020 <- data_zona %>% filter(Month == "12", Year == "2020")
+View(data_zona_dec2020)
+data_wash_dec2020 <- data_wash %>% filter(Month == "12", Year == "2020")
+View(data_wash_dec2020)
+data_miss_dec2020 <- data_miss %>% filter(Month == "12", Year == "2020")
+View(data_miss_dec2020)
+
+# trying to mutate the data
 dat_mar2021_totals <- mutate(dat_mar2021, Provisional.Drug.Overdose.Deaths <-)
 
 # (Did you remember to add your name to this script?)
